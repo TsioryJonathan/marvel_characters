6125a80a44152ac32059e311d7e784560e939cc0
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-08-08 20:08:47 +0300

    feat: update image link

--- frontend/src/assets/assets.ts
+++ frontend/src/assets/assets.ts
@@ -4,6 +4,7 @@ import spiderman from "./spiderman.jpeg";
 import ironman from "./ironman.jpeg";
 import captainmarvel from "./captainmarvel.jpeg";
 import blackpanther from "./blackpanther.jpeg";
+
 export const assets = {
   marvelLogo,
   heroImage,
